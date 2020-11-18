1f0e104f99c15c5752f0b525125c557c3f6c63f6
Felipe Cunha <felipe.cunha@afya.com.br>
2020-11-18 09:41:05 -0300

    add user on session controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const SessionController = require('./controllers/SessionController')
 
 // req.query = acessar query params (filtros)
 // ex: /users?idade=24 -> req.query.idade
@@ -9,8 +10,6 @@ const routes = express.Router()
 
 // req.body = acessar corpo da requisicao (criacao, edicao)
 
-routes.post('/users', (req, res) => {
-  return res.json(req.body)
-})
+routes.post('/sessions', SessionController.store)
 
 module.exports = routes
