725299246ac5c572ec3a6d78975d17335d0686e3
Felipe Cunha <felipe.cunha@afya.com.br>
2020-11-18 19:49:08 -0300

    add login screen registering user on db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,48 @@
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
+import logo from './assets/logo.svg'
+
 function App() {
+
+  const [email, setEmail] = useState('');
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await api.post('/sessions', { email });
+    
+    const { _id } = res.data;
+    
+    localStorage.setItem('user', _id);
+  }
+  
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='container'>
+      <img src={logo} alt='AirCnC' />
+
+      <div className='content'>
+        <p>
+          Ofereça <strong>spots</strong> para programadorese e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='email'>E-MAIL * </label>
+          <input 
+            id='email' 
+            type='email'
+            placeholder='Seu melhor e-mail'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+
+          <button 
+            type='submit'
+            className='btn'
+          >Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
