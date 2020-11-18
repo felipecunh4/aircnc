b4f343f6e7f7c665c440d4466b0a1845cae1010d
Felipe Cunha <felipe.cunha@afya.com.br>
2020-11-18 10:18:06 -0300

    add spots controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const routes = express.Router()
-const SessionController = require('./controllers/SessionController')
 
-// req.query = acessar query params (filtros)
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+
+const SessionController = require('./controllers/SessionController')
+const SpotController = require('./controllers/SpotController')
+
+const routes = express.Router()
+const upload = multer(uploadConfig)
+
 // ex: /users?idade=24 -> req.query.idade
+// req.query = acessar query params (filtros)
 
 // req.params = acessar route params (edicao, delete)
 // ex: /users/:id -> req.params.id
@@ -11,5 +18,8 @@ const SessionController = require('./controllers/SessionController')
 // req.body = acessar corpo da requisicao (criacao, edicao)
 
 routes.post('/sessions', SessionController.store)
+
+routes.get('/spots', SpotController.index)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store)
 
 module.exports = routes
